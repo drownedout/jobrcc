11c685a72a87b0fa21a10fc38a6b9b97748247e5
drownedout <danielcdonnelly@gmail.com>
2018-10-04 21:01:31 -0700

    Added eslint as dev dependency and linted files

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "extends": "standard",
+    "rules": {
+		"no-tabs": 0,
+		"indent": [2, "tab"]
+	},
+};
--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const express = require('express');
-var path = require('path');
-const port = 3000;
+const express = require('express')
+var path = require('path')
+const port = 3000
 const routes = require('./routes/index')
 
 // Initialize App
-const app = express();
+const app = express()
 
 // Public Folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
 // View Engine - EJS
-app.set('views', path.join(__dirname, 'views'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+app.set('views', path.join(__dirname, 'views'))
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html')
 
 // Routes
-app.use('/', routes);
+app.use('/', routes)
 
-app.listen(port, () => console.log(`App is listening on port ${port}`));
+app.listen(port, () => console.log(`App is listening on port ${port}`))
